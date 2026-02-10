a8ad5a4601fbac442cfce7a994e2a4d64de30cbe
Misha Opstal <mishie.opstal@gmail.com>
2026-02-10 16:40:06 +0100

    Refactor audio asset paths: update base paths for audio and video files and add function to associate audio with corresponding video

--- helpers/audio_helper.py
+++ helpers/audio_helper.py
@@ -1,11 +1,13 @@
 import os
 
-BASE_PATH = "assets/sounds"
+ASSET_PATH = "/app/assets"
+AUDIO_PATH = os.path.join(ASSET_PATH, "sounds")
+MOVIE_PATH = os.path.join(ASSET_PATH, "movies")
 
-CALL_PATH = os.path.join(BASE_PATH, "telephone", "call")
-HANG_UP_PATH = os.path.join(BASE_PATH, "telephone", "hang_up")
-ANSWER_PATH = os.path.join(BASE_PATH, "answers")
-YAPPING_PATH = os.path.join(BASE_PATH, "yapping")
+CALL_PATH = os.path.join(AUDIO_PATH, "telephone", "call")
+HANG_UP_PATH = os.path.join(AUDIO_PATH, "telephone", "hang_up")
+ANSWER_PATH = os.path.join(AUDIO_PATH, "answers")
+YAPPING_PATH = os.path.join(AUDIO_PATH, "yapping")
 
 def get_audio_files(path: str) -> list[str]:
     if not os.path.isdir(path):
@@ -16,3 +18,25 @@ def get_audio_files(path: str) -> list[str]:
         for f in os.listdir(path)
         if f.lower().endswith(".mp3")
     ]
+
+def get_associated_video(audio_path: str):
+    if not os.path.isfile(audio_path):
+        return None
+
+    # Look for mp4 with same name in /app/assets/movies
+    base_name = os.path.basename(audio_path)
+    relative_path = audio_path.replace("/app/assets/sounds/", "")
+    video_name = base_name.replace(".mp3", ".mp4")
+    video_path = os.path.join(MOVIE_PATH, relative_path, video_name)
+    if os.path.isfile(video_path):
+        return video_path
+
+    # If the path includes call we know it is the scripted phone pickup, play pickup.mp4
+    if "call" in relative_path:
+        return os.path.join(MOVIE_PATH, relative_path, "pickup.mp4")
+
+    # If the path includes hang_up we know it is the scripted phone hangup, play hangup.mp4
+    if "hang_up" in relative_path:
+        return os.path.join(MOVIE_PATH, relative_path, "hangup.mp4")
+
+    return None
